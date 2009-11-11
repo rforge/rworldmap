0e512e952af075c4e9b5c411487d61b511763c3b
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-11 13:15:04 +0000

    added identifyCountries() for interactive identification of countries and attribute values
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rworldmap@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/rworldmap/R/identifyCountries.r
@@ -0,0 +1,52 @@
+identifyCountries <- function(dF=""
+                             ,nameCountryColumn="NAME"
+                             ,nameX="LON"
+                             ,nameY="LAT"
+                             ,nameColumnToPlot=""
+                             ,plotSelected=FALSE
+                             ,...){
+
+#! ANDY TO DO 11/11/09
+#add checks that the input parameters are valid
+
+
+#also possible to get centroids from a sPDF
+if (class(dF)=="SpatialPolygonsDataFrame")
+   {
+    centroidCoords <- coordinates(getMap())
+    #within this function just need the dF bit of the sPDF
+    dF <- dF@data
+    #adding extra attribute columns to contain centroids (even though such columns may already be there)
+    dF[['nameX']] <- centroidCoords[,1]
+    dF[['nameY']] <- centroidCoords[,2]    
+    nameX <- 'nameX'
+    nameY <- 'nameY'    
+   } else
+#this assumes that the dF already has columns for lat & lon
+if (class(dF)=="data.frame")
+   {
+    #this assumes that nameX, nameY & nameCountryColumn columns have been passed correctly
+
+   } else
+   {
+    #if no object is passed use the internal map
+    dF <- getMap()@data
+    nameX <- 'LON'
+    nameY <- 'LAT'    
+   }
+
+labels <- dF[[nameCountryColumn]]
+
+#if an attribute column name is passed paste it's value onto end of country label
+if ( nameColumnToPlot != "" ) labels <- paste(labels,dF[[nameColumnToPlot]]) 
+
+selectedCountryIndices <- identify(x=dF[[nameX]], y=dF[[nameY]], labels=labels,...)
+
+#allowing plotting of the boundaries of the selected countries
+#this is really just an initial test of something I may develop further later
+if (plotSelected) plot(sPDF[selectedCountryIndices,],border='blue',add=TRUE)
+
+#return the indices, may be useful later
+invisible(selectedCountryIndices)
+                           
+} #end of identifyCountries

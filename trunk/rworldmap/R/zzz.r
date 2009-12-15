21eb85b92c2de21473386fc9fe54a0b0d9b72567
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-15 15:23:59 +0000

    changed startup recomended help command to : help(rworldmap,help_type='html')
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rworldmap@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/rworldmap/R/zzz.r
+++ trunk/rworldmap/R/zzz.r
@@ -1,6 +1,7 @@
 `.First.lib`<-
 function(libname,pkgname){
 cat("Welcome to rworldmap\n")
-cat("For help files type :           \t ?rworldmap\n")
+#cat("For help files type :           \t ?rworldmap\n")
+cat("For help files type :           \t help(rworldmap,help_type='html')\n")
 cat("For a short introduction type : \t vignette('rworldmap')\n")
 }

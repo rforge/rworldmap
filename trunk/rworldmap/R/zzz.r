9548ab5219c6d06a7be5d3e180fab9151a02b857
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-26 00:30:14 +0000

    ~ altered commands to packageStartupMessage in zzz.r
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rworldmap@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/rworldmap/R/zzz.r
+++ trunk/rworldmap/R/zzz.r
@@ -1,7 +1,11 @@
 `.First.lib`<-
 function(libname,pkgname){
-cat("### Welcome to rworldmap ###\n")
+#cat("### Welcome to rworldmap ###\n")
 #cat("For help files type :           \t ?rworldmap\n")
-cat("For help files type :           \t help(rworldmap,help_type='html')\n")
-cat("For a short introduction type : \t vignette('rworldmap')\n")
+#cat("For help files type :           \t help(rworldmap,help_type='html')\n")
+#cat("For a short introduction type : \t vignette('rworldmap')\n")
+
+packageStartupMessage("### Welcome to rworldmap ###")
+packageStartupMessage("For help files type :           \t help(rworldmap,help_type='html')")
+packageStartupMessage("For a short introduction type : \t vignette('rworldmap')")
 }

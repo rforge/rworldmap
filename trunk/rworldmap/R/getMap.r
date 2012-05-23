e63d02acb898aaefd0eb7305242eb82dc62b6e5b
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-23 17:06:54 +0000

    ~ removed projection argument from getMap ~ got joinCountryData2Map.R to cope with trailing spaces in the data ~ changed version numbers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rworldmap@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/rworldmap/R/getMap.r
+++ trunk/rworldmap/R/getMap.r
@@ -1,36 +1,38 @@
-#30/7/2010 maybe add another option to access other maps
-#or just allow to be passed in the resolution param ?
-#option for map would be better
-#it could assume 'worldCountries' by default
+#
 
 `getMap` <-
 
-function(resolution="low",projection="none"){
+#function(resolution="low",projection="none"){
+function(resolution="low"){
+  
+  #23/5/12 3 new resolutions
+  #scrapped projections
+  
 
+
+  #coarsest resolution map
+ if (resolution == "coarse") {
+    #data("wrld_coarse", envir = environment(),package = "rworldmap")
+    #mapWithData <- get("wrld_coarse")
+    data("countriesCoarse", envir = environment(),package = "rworldmap")
+    mapWithData <- get("countriesCoarse")    
+  }  else if (resolution == "low") {
+    #data("wrld_simpl", envir = environment(), package = "rworldmap")
+    #mapWithData <- get("wrld_simpl")
+    data("countriesLow", envir = environment(),package = "rworldmap")
+    mapWithData <- get("countriesLow")    
+  }
+#  else if (projection == "EqualArea" || projection == "equalArea") {
+#    data("wrld_simpl_Mollweide", envir = environment(), package = "rworldmap")
+#    mapWithData <- get("wrld_simpl_Mollweide")
+#  }
+ 
   #trying eez map : does work - temporarily removed 31/8 to get permission
   #if (resolution == "eez") {
   #  data("eezMap", envir = environment(), package = "rworldmap")
   #  mapWithData <- get("eezMap")
   #} 
-  #else 
-  if (projection == "none" & resolution == "low") {
-    data("wrld_simpl_lessIslands", envir = environment(),package = "rworldmap")
-    mapWithData <- get("wrld_simpl_lessIslands")
-  }
-  #coarsest resolution map
-  else if (projection == "none" & resolution == "coarse") {
-    data("wrld_coarse", envir = environment(),package = "rworldmap")
-    mapWithData <- get("wrld_coarse")
-  }  
-  else if (projection == "none") {
-    data("wrld_simpl", envir = environment(), package = "rworldmap")
-    mapWithData <- get("wrld_simpl")
-  }
-  else if (projection == "EqualArea" || projection == "equalArea") {
-    data("wrld_simpl_Mollweide", envir = environment(), package = "rworldmap")
-    mapWithData <- get("wrld_simpl_Mollweide")
-  }
- 
+  #else
   
   return(mapWithData)
 }

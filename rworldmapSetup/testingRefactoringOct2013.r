858570f719c509a0992ad65bc0dbb3e5215ff241
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-19 18:09:46 +0000

    *mapBars corrected longstanding bug allowing xlim,ylim to be set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rworldmap@212 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- rworldmapSetup/testingRefactoringOct2013.r
+++ rworldmapSetup/testingRefactoringOct2013.r
@@ -6,12 +6,20 @@
 
 #and fixing xlim,ylim in mapPies(), mapBars()
 
+
+#check rworldmap version : CRAN 1.3-1 Dev 1.3-2
+#use make install from DOS, & restart R to install dev version
+packageDescription('rworldmap')$Version
+
+
 mapPies()
 #Error in class(dF) : 'dF' is missing
 
 #getting example data
 dF <- getMap()@data 
 sPDF <- getMap()
+
+x11()
 
 mapPies( dF,nameX="LON", nameY="LAT"
          , nameZs=c('POP_EST','POP_EST','POP_EST','POP_EST'),mapRegion='africa' )
@@ -28,19 +36,50 @@ mapBars( dF, nameX="LON", nameY="LAT"
 #Error in `[<-.data.frame`(`*tmp*`, nameZs, value = list(POP_EST = c(0.0212159904908377,  : 
 #duplicate subscripts for columns
 
-#works
-mapBars( sPDF, 
-         , nameZs=c('POP_EST','GDP_MD_EST')
-         , mapRegion='africa'
-         , symbolSize=4 )
 
-#trying xlim, ylim, they are discarded!
-mapBars( sPDF, 
-         , nameZs=c('POP_EST','GDP_MD_EST')
-         , xlim=c(-20,80), ylim=c(-30,30)
-         , symbolSize=4 )
 
 #TO DO
 #put rwmCheckAndLoadInput() into mapBars() & see if it fixes xlim,ylim
 #then can do same for mapPies()
+
+
+## WORKING ON NOW 19/3/2014 ----
+#works
+mapBars( sPDF 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , mapRegion='africa'
+         , symbolSize=4 )
+
+#now works sPDF xlim, ylim used
+mapBars( sPDF 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , xlim=c(-20,80), ylim=c(-30,30)
+         , symbolSize=4 )
+
+#now works dF xlim, ylim used
+mapBars( dF, nameX="LON", nameY="LAT" 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , xlim=c(-20,80), ylim=c(-30,30)
+         , symbolSize=4 )
+
+
+#fails
+#Error in seq.default(cumulatProps[sliceNum], cumulatProps[sliceNum + 1],  : 
+#                       'to' cannot be NA, NaN or infinite
+mapPies( sPDF 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , mapRegion='africa'
+         , symbolSize=4 )
+
+#now works sPDF xlim, ylim used
+mapPies( sPDF 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , xlim=c(-20,80), ylim=c(-30,30)
+         , symbolSize=4 )
+
+#now works dF xlim, ylim used
+mapPies( dF, nameX="LON", nameY="LAT" 
+         , nameZs=c('POP_EST','GDP_MD_EST')
+         , xlim=c(-20,80), ylim=c(-30,30)
+         , symbolSize=4 )
 
